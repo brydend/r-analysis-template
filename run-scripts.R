d4a22a32c35b4dd6afad8d44c749c26a0d8920a0
brydend <danielbryden@gmail.com>
2020-12-12 08:32:57 +0000

    Update run-scripts.R

--- run-scripts.R
+++ run-scripts.R
@@ -1,3 +1,38 @@
 # Enter all scripts that need to be run.
 
 # This could run in parallel with a markdown file, although in theory not.
+
+# Cycle through valid scripts in each valid folder.
+
+my.files <- files.list("extract")
+
+for (source.file in my.files) {
+
+  source("source.file")
+
+}
+
+my.files <- files.list("transform")
+
+for (source.file in my.files) {
+
+  source("source.file")
+
+}
+
+my.files <- files.list("analysis")
+
+for (source.file in my.files) {
+
+  source("source.file")
+
+}
+
+my.files <- files.list("load")
+
+for (source.file in my.files) {
+
+  source("source.file")
+
+}
+
